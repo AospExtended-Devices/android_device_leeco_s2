davisson2003/android_device_c106

s2: Use COMMON_LUNCH_CHOICES for adding build to lunch combo list
* adding throuh vendorsetup.sh is obsolete now

Signed-off-by: Akash Mondal <a98mondal@gmail.com>

AndroidProducts.mk
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/arrow_s2.mk
+
+COMMON_LUNCH_CHOICES := \
+    arrow_s2-user \
+    arrow_s2-userdebug \
+    arrow_s2-eng

AospExtended-Devices/android_device_leeco_s2

AndroidProducts.mk
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_s2.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_s2-user \
+    aosp_s2-userdebug \
+    aosp_s2-eng